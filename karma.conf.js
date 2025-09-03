d8aaabedd6e4f8d82e3aaf1a4b1d07a608312953
Francis <francisleborgne@gmail.com>
2025-09-03 12:07:09 +0200

    update4

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-firefox-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Firefox'],
+    singleRun: true,
+    restartOnFileChange: false
+  });
+};

a577827297aa72834df7c3a9aa2b5998a87c2520
Francis <francisleborgne@gmail.com>
2025-09-03 14:26:07 +0200

    update7

--- karma.conf.js
+++ karma.conf.js
@@ -2,20 +2,40 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
     plugins: [
       require('karma-jasmine'),
-      require('karma-firefox-launcher'),
+      require('karma-chrome-launcher'), // ajouté pour Chrome
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
     reporters: ['progress', 'kjhtml'],
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['FirefoxHeadless'],
     singleRun: true,
-    restartOnFileChange: false
+    restartOnFileChange: false,
+
+    browsers: ['ChromeHeadless'], // <-- Chrome en mode headless
+
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+      }
+    }
   });
 };
